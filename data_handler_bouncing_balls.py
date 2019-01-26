8250370e27257a88d6a1488cdbee1f47666d4706
David Sandberg <david.o.sandberg@gmail.com>
2019-01-26 13:52:49 +0100

    Small clean-up

--- data_handler_bouncing_balls.py
+++ data_handler_bouncing_balls.py
@@ -4,29 +4,22 @@ This script comes from the RTRBM code by Ilya Sutskever from
 http://www.cs.utoronto.ca/~ilya/code/2008/RTRBM.tar
 """
 
-#from numpy import *                
 import sys
 import numpy as np
-from scipy import *               
-import pdb
-#import cPickle as pickle
-import pickle
-import scipy.io
+import scipy
+import os
+import utils
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-shape_std=shape
+shape_std=scipy.shape
 def shape(A):
-    if isinstance(A, ndarray):
+    if isinstance(A, scipy.ndarray):
         return shape_std(A)
     else:
         return A.shape()
 
-size_std = size
+size_std = scipy.size
 def size(A):
-    if isinstance(A, ndarray):
+    if isinstance(A, scipy.ndarray):
         return size_std(A)
     else:
         return A.size()
@@ -39,22 +32,22 @@ def new_speeds(m1, m2, v1, v2):
     return new_v1, new_v2
     
 
-def norm(x): return sqrt((x**2).sum())
-def sigmoid(x):        return 1./(1.+exp(-x))
+def norm(x): return scipy.sqrt((x**2).sum())
+def sigmoid(x):        return 1./(1.+scipy.exp(-x))
 
 SIZE=10
 # size of bounding box: SIZE X SIZE.
 
 def bounce_n(T=128, n=2, r=None, m=None):
-    if r is None: r=array([1.2]*n)
-    if m==None: m=array([1]*n)
+    if r is None: r=scipy.array([1.2]*n)
+    if m==None: m=scipy.array([1]*n)
     # r is to be rather small.
-    X=zeros((T, n, 2), dtype='float')
-    v = randn(n,2)
+    X=scipy.zeros((T, n, 2), dtype='float')
+    v = scipy.randn(n,2)
     v = v / norm(v)*.5
     good_config=False
     while not good_config:
-        x = 2+rand(n,2)*8
+        x = 2+scipy.rand(n,2)*8
         good_config=True
         for i in range(n):
             for z in range(2):
@@ -75,7 +68,7 @@ def bounce_n(T=128, n=2, r=None, m=None):
         for i in range(n):
             X[t,i]=x[i]
             
-        for mu in range(int(1/eps)):
+        for _ in range(int(1/eps)):
 
             for i in range(n):
                 x[i]+=eps*v[i]
@@ -93,8 +86,8 @@ def bounce_n(T=128, n=2, r=None, m=None):
                         w    = x[i]-x[j]
                         w    = w / norm(w)
 
-                        v_i  = dot(w.transpose(),v[i])
-                        v_j  = dot(w.transpose(),v[j])
+                        v_i  = scipy.dot(w.transpose(),v[i])
+                        v_j  = scipy.dot(w.transpose(),v[j])
 
                         new_v_i, new_v_j = new_speeds(m[i], m[j], v_i, v_j)
                         
@@ -104,70 +97,54 @@ def bounce_n(T=128, n=2, r=None, m=None):
     return X
 
 def ar(x,y,z):
-    return z/2+arange(x,y,z,dtype='float')
+    return z/2+scipy.arange(x,y,z,dtype='float')
 
 def matricize(X,res,r=None):
 
     T, n= shape(X)[0:2]
-    if r is None: r=array([1.2]*n)
+    if r is None: r=scipy.array([1.2]*n)
 
-    A=zeros((T,res,res), dtype='float')
+    A=scipy.zeros((T,res,res), dtype='float')
     
-    [I, J]=meshgrid(ar(0,1,1./res)*SIZE, ar(0,1,1./res)*SIZE)
+    [I, J]=scipy.meshgrid(ar(0,1,1./res)*SIZE, ar(0,1,1./res)*SIZE)
 
     for t in range(T):
         for i in range(n):
-            A[t]+= exp(-(  ((I-X[t,i,0])**2+(J-X[t,i,1])**2)/(r[i]**2)  )**4    )
+            A[t]+= scipy.exp(-(  ((I-X[t,i,0])**2+(J-X[t,i,1])**2)/(r[i]**2)  )**4    )
             
         A[t][A[t]>1]=1
     return A
 
-def bounce_mat(res, n=2, T=128, r =None):
-    if r==None: r=array([1.2]*n)
-    x = bounce_n(T,n,r);
-    A = matricize(x,res,r)
-    return A
-
 def bounce_vec(res, n=2, T=128, r =None, m =None):
-    if r==None: r=array([1.2]*n)
+    if r==None: r=scipy.array([1.2]*n)
     x = bounce_n(T,n,r,m);
     V = matricize(x,res,r)
     return V.reshape(T, res**2)  
 
-def save_as_pickle(filename, arr):
-    with open(filename, 'wb') as f:
-        pickle.dump(arr, f)
-
-# make sure you have this folder
-logdir = './sample'
-def show_sample(V):
-    T   = len(V)
-    res = int(sqrt(shape(V)[1]))
-    for t in range(T):
-        plt.imshow(V[t].reshape(res,res),cmap=matplotlib.cm.Greys_r) 
-        # Save it
-        fname = logdir+'/'+str(t)+'.png'
-        plt.savefig(fname)      
-        
 if __name__ == "__main__":
     res=80
     T=20
-    N=1000
+    N=100
+    M=200
+    target_dir = os.path.join('data', 'bouncing_balls_diff0p0_bae_0p0')
+    os.makedirs(target_dir, exist_ok=True)
     nrof_balls = 1
-    for j in range(20):
+    for j in range(M):
         print('.', end='')
         sys.stdout.flush()
-        dat=empty((N), dtype=object)
+        dat=scipy.empty((N), dtype=object)
         for i in range(N):
             dat[i]=bounce_vec(res=res, n=nrof_balls, T=T)
-        data = np.reshape(stack(dat), (N, T, res, res))
-        save_as_pickle('data/bouncing_balls_training_data_%03d.pkl' % j, data)
+        data = np.reshape(scipy.stack(dat), (N, T, res, res))
+        utils.save_pickle(os.path.join(target_dir, 'train_%03d.pkl' % j), data)
     print('\nDone')
     
-    N=1000
-    dat=empty((N), dtype=object)
-    for i in range(N):
-        dat[i]=bounce_vec(res=res, n=nrof_balls, T=T)
-    data = np.reshape(stack(dat), (N, T, res, res))
-    save_as_pickle('data/bouncing_balls_testing_data.pkl', data)
+    N=100
+    M=10
+    dat=scipy.empty((N), dtype=object)
+    for j in range(M):
+        for i in range(N):
+            dat[i]=bounce_vec(res=res, n=nrof_balls, T=T)
+        data = np.reshape(scipy.stack(dat), (N, T, res, res))
+        utils.save_pickle(os.path.join(target_dir, 'test_%03d.pkl' % j), data)
 
